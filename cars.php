040576ac33479f970bfc2169d7cad7dcdfa34014
Szasza0012 <szaszovszki.kevin.janos2002@tanulo.boronkay.hu>
2023-11-27 11:16:19 +0100

    getMakers

--- cars.php
+++ cars.php
@@ -5,7 +5,7 @@ $FileName  = "car-db.csv";
 $csvData = getCsvData($FileName);
 $result = [];
 $maker = [];
-$Maker = [];
+$makers = [];
 $header = $csvData[0];
 $idxMaker = array_search ('make', $header);
 $idxModel = array_search ('model', $header);
@@ -28,7 +28,7 @@ function getCsvData($FileName){
     return $lines;
 }
  
-if (empty($csvData)) {
+/*if (empty($csvData)) {
     echo "Nincs adat!";
     return false;
 }
@@ -43,12 +43,48 @@ foreach ($csvData as $idx => $line) {
     }
     if ($maker != $line[$idxMaker]){
         $maker = $line[$idxMaker];
-        $Maker[] = $maker;
+        //$makers[] = $maker;
     }
     if ($model != $line[$idxModel]){
         $model = $line[$idxModel];
         $result[$maker][] = $model;
     }
     }
-    //print_r($result);
-    print_r($Maker);
+  */
+function getMakers($csvData){
+    if (empty($csvData)) {
+        echo "Nincs adat!";
+        return false;
+    }
+    $maker = '';
+    $header = $csvData[0];
+    $idxMaker = array_search ('make', $header);
+    foreach ($csvData as $idx => $line) {
+        if(!is_array($line)){
+            continue;
+        }
+        if ($idx == 0) {
+            continue;
+        }
+        if ($maker != $line[$idxMaker]){
+            $maker = $line[$idxMaker];
+            $makers[] = $maker;
+        }
+    }
+    return $makers;
+}
+
+$makers = getMakers($csvData);
+print_r($makers);
+
+
+
+
+
+/*$mysqli = new mysqli("localhost","-u root"," ","cars");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}*/
